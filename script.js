17597b65883e6b46c6d5665ada35a7761397508b
Florencia Morisse <florenciamorisse@gmail.com>
2022-12-01 15:13:38 -0300

    carpeta components

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-( ()=>{
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
@@ -22,35 +24,5 @@ const createTask = (evento) => {
 }
 btn.addEventListener("click", createTask);
 
-const checkComplete = ()=>{
-    const i = document.createElement("i");
-    i.classList.add('far','fa-check-square', 'icon');
-    i.addEventListener("click", completeTask);
-    return i 
-}
-const completeTask = (event)=>{
-    const element= event.target;
-    element.classList.toggle("far");
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-}    
 
-const deleteIcon = ()=>{
-    const i = document.createElement("i");
-    i.classList.add("fas","fa-trash-alt","trashIcon","icon");   
-    i.addEventListener("click", deleteTask);
-    return i ;
-}
-const deleteTask=(event)=>{
-    const parent = event.target.parentElement.parentNode;
-    parent.remove();
-    
-    // const parent = event.parentElement;
-    // console.log(parent)
-    // parent.remove()
 
-    
-
-}
-
-})()
